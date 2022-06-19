4a7e94dc9249708d96940cae3f49e61596c8050a
Lexx <93395794+JLex11@users.noreply.github.com>
2022-06-19 12:28:53 -0500

    commit

---
+++ Connection/conexion.php
@@ -0,0 +1,6 @@
+<?php
+// $cnn = nombre de la variable de conexion
+// new mysqli = cargar los comandos de trabajo sql y orientado a objetos
+$cnn = new mysqli("localhost", "root", "", "Cinemax");
+
+?>
