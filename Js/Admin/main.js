1447bf732eb351f0d1f2e4d01fc7a5ac4d8939c1
Lexx <93395794+JLex11@users.noreply.github.com>
2022-06-20 22:38:33 +0000

    chart line

--- Js/Admin/main.js
+++ Js/Admin/main.js
@@ -199,7 +199,7 @@ function crearGrafico() {
         let canvasGrafico = document.getElementById(contenedor);
         // eslint-disable-next-line no-undef
         new Chart(canvasGrafico, {
-            type: "pie",
+            type: "line",
             data: {
                 labels: parametros,
                 datasets: [
